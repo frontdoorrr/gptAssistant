78d917d302ab66ee7aa43f04a3c44ab5b3d7a905
JEONGMUN LEE <lot9616@gmail.com>
2024-01-13 02:39:50 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+
+import openai
+
+api_key = os.environ.get(
+    # "OPENAI_API_KEY", "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"
+)
+
+client = openai.OpenAI(api_key=api_key)
+
+
+# class GPTClient:
+#     def __init__(self, api_key) -> None:
+#         self.api_key = api_key
+#         self.client = openai.OpenAI()
+
+#     def _create_assistant(self):
+#         return
+
+#     def _create_thread(self):
+#         return
+
+#     def _create_messeage(self):
+#         return
+
+#     def _receive_message(self):
+#         self.client
+
+assistant = client.beta.assistants.create(
+    name="테스트 봇",
+    instructions="개발 테스트를 위해 만들었습니다",
+    tools=[
+        {
+            "type": "retrieval",
+        }
+    ],
+    model="gpt-4-1106-preview",
+)
+
+print(type(assistant), assistant)
+
+# File upload
+file1 = client.files.create(file=open("", "rb"), purpose="assistants")
+file2 = client.files.create(file=open("", "rb"), purpose="assistants")
+
+# Create thread
+thread = client.beta.threads.create()
+
+# Create Message
+
+message = client.beta.threads.messages.create()
+
+# Create Response
+
+run = client.beta.threads.run.create(thread_id=thread.id, assistant_id=assistant.id)
