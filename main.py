4952cf588933d3cd742b01693ff860744b9ffda2
JEONGMUN LEE <lot9616@gmail.com>
2024-01-17 22:51:36 +0900

    [Fix] 수정

--- main.py
+++ main.py
@@ -14,69 +14,98 @@ def get_json(setting, secrets=secrets):
 
 
 OPENAI_API_KEY = get_json("OPENAI_API_KEY")
-# FILE_PATHS = os.environ.get("FILE_PATHS")
-# FILE_PATHS = FILE_PATHS.split(",")
-print(OPENAI_API_KEY)
+FILE_PATHS = get_json("FILE_PATHS")
+FILE_PATHS = FILE_PATHS.split(",")
 
-# client = openai.OpenAI(api_key=OPENAI_API_KEY)
-
-
-# class GPTClient:
-#     def __init__(self, api_key) -> None:
-#         self.api_key = api_key
-#         self.client = openai.OpenAI()
-
-#     def _create_assistant(self):
-#         return
-
-#     def _create_thread(self):
-#         return
-
-#     def _create_messeage(self):
-#         return
-
-#     def _receive_message(self):
-#         self.client
-
-# assistant = client.beta.assistants.create(
-#     name="테스트 봇",
-#     instructions="개발 테스트를 위해 만들었습니다",
-#     tools=[
-#         {
-#             "type": "retrieval",
-#         }
-#     ],
-#     model="gpt-4-1106-preview",
-# )
-
-# print(type(assistant), assistant)
-
-# # File upload
-# file1 = client.files.create(file=open("", "rb"), purpose="assistants")
-# file2 = client.files.create(file=open("", "rb"), purpose="assistants")
-
-# # Create thread
-# thread = client.beta.threads.create()
-
-# # Create Message
-
-# message = client.beta.threads.messages.create()
-
-# # Create Response
-
-# run = client.beta.threads.run.create(
-#     thread_id=thread.id,
-#     assistant_id=assistant.id,
-# )
+client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
 
-# # Print Response
+# # class GPTClient:
+# #     def __init__(self, api_key) -> None:
+# #         self.api_key = api_key
+# #         self.client = openai.OpenAI()
 
-# run = client.beta.threads.run.retrieve(
-#     thread_id=thread.id,
-#     run_id=run.id,
-# )
-# messages = client.beta.threads.messages.list(thread_id=thread.id)
+# #     def _create_assistant(self):
+# #         return
+
+# #     def _create_thread(self):
+# #         return
+
+# #     def _create_messeage(self):
+# #         return
+
+# #     def _receive_message(self):
+# #         self.client
+
+# File upload
+file_ids = []
+for filepath in FILE_PATHS:
+    file = client.files.create(
+        file=open(filepath, "rb"),
+        purpose="assistants",
+    )
+    file_ids.append(file.id)
+print("*****", file_ids)
+
+
+assistant = client.beta.assistants.create(
+    name="커피 테스트 Assistant",
+    instructions="커피 원두와 관련된 정보를 알려줍니다.",
+    tools=[
+        {
+            "type": "retrieval",
+        },
+        {
+            "type": "code_interpreter",
+        },
+        # {
+        #     "type": "function",
+        # },
+    ],
+    file_ids=file_ids,
+    model="gpt-4-1106-preview",
+)
+
+print(type(assistant), assistant)
+
+
+# Create thread
+thread = client.beta.threads.create()
+print("Thread :", thread)
+# Create Message
+
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    content="가성비가 좋은 원두에 대해 알려주세요",
+    # file_ids=file_ids,
+    role="user",
+)
+
+print("Message :", message)
+# Create Response
+
+run = client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant.id,
+)
+print("Run :", run)
+
+# Print Response
+
+run = client.beta.threads.runs.retrieve(
+    thread_id=thread.id,
+    run_id=run.id,
+)
+
+print("Run :", run)
+
+messages = client.beta.threads.messages.list(thread_id=thread.id)
+print("Messages  : ", messages)
 # message = [message.content[0].text for message in messages if message.run_id == run.id][
 #     0
 # ]
+
+
+for message in reversed(messages.data):
+    print(message.role + " : ", message.content[0].text.value)
+# messages.data[0].content
